46a41f0cc031d927cd342a41f9e537b0837a09d3
Yrjö Kari-Koskinen <ykk@peruna.fi>
2022-09-19 21:38:21 +0300

    Move weather API code to a separate namespace

--- src/hello_aurajoki/main.clj
+++ src/hello_aurajoki/main.clj
@@ -2,11 +2,10 @@
   (:gen-class)
   (:require [aleph.http :as http]
             [reitit.ring :as ring]
-            [clj-commons.byte-streams :as bs]
-            [jsonista.core :as j]
             [reitit.ring.middleware.muuntaja :as muuntaja]
             [reitit.ring.middleware.parameters :as parameters]
-            [muuntaja.core :as m])
+            [muuntaja.core :as m]
+            [hello-aurajoki.forecast :as forecast])
   (:import [java.time OffsetDateTime]))
 
 ;; Handlers
@@ -22,49 +21,11 @@
                 "!\n\n"
                 (OffsetDateTime/now))}))
 
-; https://open-meteo.com/en/docs
-(def forecast-url-prefix
-  "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m&")
-; https://open-meteo.com/en/docs/geocoding-api
-(def geocoding-url-prefix
-  "https://geocoding-api.open-meteo.com/v1/search?name=")
-
-(defn- api-get [url]
-  (-> @(http/get url)
-    :body
-    bs/to-string
-    (j/read-value j/keyword-keys-object-mapper)))
-
-(defn- get-location [city-name]
-  (-> (str geocoding-url-prefix city-name)
-      api-get
-      :results
-      first))
-
-(defn- average-temperature-forecast [{:keys [latitude longitude name
-                                             country_code elevation]}]
-  (let [forecast-url (str forecast-url-prefix
-                          "latitude=" latitude
-                          "&longitude=" longitude)
-        response (api-get forecast-url)
-        temperatures (-> response
-                         :hourly
-                         :temperature_2m)
-        times (-> response
-                  :hourly
-                  :time
-                  sort)]
-    {:average-temperature-forecast (/ (reduce + temperatures) (count temperatures))
-     :time-start (first times)
-     :time-end (last times)
-     :city name
-     :elevation elevation
-     :country country_code}))
 
 (defn- forecast-handler [{:keys [path-params]}]
-  (if-let [location (get-location (:city path-params))]
+  (if-let [location (forecast/get-location (:city path-params))]
     {:status 200
-     :body (average-temperature-forecast location)}
+     :body (forecast/average-temperature-forecast location)}
     {:status 404
      :body {:error (str (:city path-params) " not found!")}}))
 
