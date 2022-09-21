32441cb74ef6c65ef6581f5558e1439af03f91b1
Yrjö Kari-Koskinen <ykk@peruna.fi>
2022-09-21 21:36:18 +0300

    Live coding at Aurajoki overflow meetup

--- src/hello_aurajoki/main.clj
+++ src/hello_aurajoki/main.clj
@@ -6,18 +6,58 @@
             [jsonista.core :as j]
             [reitit.ring.middleware.muuntaja :as muuntaja]
             [reitit.ring.middleware.parameters :as parameters]
-            [muuntaja.core :as m]))
+            [muuntaja.core :as m])
+  (:import [java.time OffsetDateTime]))
 
 ;; Handlers
 
-(defn home-handler [request]
+(defn home-handler [{:keys [params] :as request}]
+  (def r request)
+  (let [friend (get params "friend")]
+    {:status 200
+     :body (str "Hello world"
+                (when friend
+                  (str " and " friend))
+                "!\n"
+                (OffsetDateTime/now))}))
+
+(def weather-url "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m&")
+(def geocoding-url "https://geocoding-api.open-meteo.com/v1/search?name=")
+
+(defn api-get [url]
+  (-> @(http/get url)
+      :body
+      bs/to-string
+      (j/read-value j/keyword-keys-object-mapper)))
+
+(defn get-location [city]
+  (-> (api-get (str geocoding-url city))
+      :results
+      first))
+
+(defn get-avg-temperature [{:keys [:latitude :longitude]}]
+  (let [response (api-get (str weather-url
+                            "latitude=" latitude
+                            "&longitude=" longitude))
+        times (some-> response
+                  :hourly
+                  :time)
+        temperatures (some-> response
+                         :hourly
+                         :temperature_2m)]
+    {:avg-temperature (/ (reduce + temperatures) (count temperatures))
+     :start-date (first times)
+     :end-date (last times)}))
+
+(defn temperature-handler [{:keys [path-params]}]
   {:status 200
-   :body "Hello world!"})
+   :body {:temperature (get-avg-temperature (get-location (:city path-params)))}})
 
 ;; Routes and middleware
 
 (def routes
-  [["/" {:get {:handler home-handler}}]])
+  [["/" {:get {:handler home-handler}}]
+   ["/api/average-temperature/:city" {:get {:handler temperature-handler}}]])
 
 (def ring-opts
   {:data
